7ffcb5e65ee64cf8ec84b35973d741a917df8680
Shrikant <sawantshrikant07@gmail.com>
2023-03-09 15:17:02 +0530

    [Shrikant] add:PerfectNumber

---
+++ LogicalProgrammingPracticeProblem/PerfectNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LogicalProgrammingPracticeProblem
+{
+    public class PerfectNumber
+    {
+        public bool IsPerfectNumber(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == num;
+        }
+    }
+}
--- LogicalProgrammingPracticeProblem/Program.cs
+++ LogicalProgrammingPracticeProblem/Program.cs
@@ -1,7 +1,7 @@
 ﻿using LogicalProgrammingPracticeProblem;
 using System;
 
-public class Program
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -9,21 +9,35 @@ public class Program
         bool flag = true;
         while (flag)
         {
-            Console.Write("1. Fibonacci\n2. Exit\nEnter your option to execute: ");
+            Console.Write("1. Fibonacci\n2. Perfect Number\n3. Exit\nEnter your option to execute: ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FibonacciSeries fib = new FibonacciSeries();
-                    Console.Write("Enter the number for Fibonacci Series: ");
+                    Console.Write("Enter the number of terms for Fibonacci Series: ");
                     int num = Convert.ToInt32(Console.ReadLine());
                     fib.PrintFibonacciSeries(num);
                     break;
                 case 2:
+                    PerfectNumber perf = new PerfectNumber();
+                    Console.Write("Enter a number to check if it's a perfect number: ");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    bool isPerfect = perf.IsPerfectNumber(input);
+                    if (isPerfect)
+                    {
+                        Console.WriteLine(input + " is a perfect number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(input + " is not a perfect number.");
+                    }
+                    break;
+                case 3:
                     flag = false;
                     break;
                 default:
-                    Console.WriteLine("Invalid option.");
+                    Console.WriteLine("Invalid option. Please try again.");
                     break;
             }
         }
