66d28ba65d9afb59c8fb8fcf559a79bfb7135e21
Shrikant <sawantshrikant07@gmail.com>
2023-03-09 15:05:41 +0530

    [Shrikant] add:FibonacciSeries

---
+++ LogicalProgrammingPracticeProblem/FibonacciSeries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LogicalProgrammingPracticeProblem
+{
+    public class FibonacciSeries
+    {
+        public void PrintFibonacciSeries(int num)
+        {
+            int firstNum = 0, secondNum = 1, fibonacciValue;
+            Console.Write(firstNum + " " + secondNum + " ");
+            for (int i = 2; i < num; i++)
+            {
+                fibonacciValue = firstNum + secondNum;
+                Console.Write(fibonacciValue + " ");
+                firstNum = secondNum;
+                secondNum = fibonacciValue;
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
